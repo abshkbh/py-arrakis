77d2c4c9fdcf4b555a8c75b44f3c43509bb9d32e
Abhishek Bhardwaj <abshkbh@gmail.com>
2025-03-08 18:30:55 +0000

    Update setup.py for PyPI release v0.1.0

--- setup.py
+++ setup.py
@@ -2,19 +2,29 @@ from setuptools import setup, find_packages
 
 setup(
     name="py-arrakis",
-    version="0.0.1",
+    version="0.1.0",
     author="Abhishek Bhardwaj",
     author_email="abshkbh@gmail.com",
-    description="SDK for https://github.com/abshkbh/arrakis",
+    description="Python SDK for the Arrakis sandboxes - https://github.com/abshkbh/arrakis",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/abshkbh/py-arrakis",
     packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+    ],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.9",
+    keywords="virtualization, sandbox, vm, testing",
 )
